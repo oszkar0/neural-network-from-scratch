1946d03a90be33ef587a8269185559242584de78
oszkar0 <oskszysiak@gmail.com>
2023-08-11 00:02:11 +0200

    creat eNeuralNetwork class

---
+++ network.py
@@ -0,0 +1,31 @@
+class NeuralNetwork:
+    def __init__(self, loss_func_derivative):
+        self.loss_func_derivative = loss_func_derivative
+        self.layers = []
+
+    def add_layer(self, layer):
+        self.layers.append(layer)
+
+    def fit(self, x_train, y_train, epochs, learning_rate):
+        for i in range(epochs):
+            for j in range(len(x_train)):
+                # calculate output
+                output = x_train[j]
+
+                for layer in self.layers:
+                    output = layer.forward_propagate(output)
+
+                # adjust the weights and biases
+                error = self.loss_func_derivative(y_train[j], output)
+
+                for layer in reversed(self.layers):
+                    error = layer.backward_propagate(error, learning_rate)
+
+    def predict(self, x_input):
+        # calculate and return output
+        output = x_input
+
+        for layer in self.layers:
+            output = layer.forward_propagate(output)
+
+        return output
